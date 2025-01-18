dbb32cce8d435480267befe0f53b48f91cde3ff1
Ah777ed_said <ahmed01226313482@gmail.com>
2025-01-18 19:10:08 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MobileNet",
-    version="0.1",
-    packages=find_packages(),  # Automatically finds all packages in the project
-    install_requires=[          # List any dependencies here
-        "tensorflow",
-        "opencv-python",
-        "numpy",
-        "requests",
+    name='image_classifier',  # اسم الحزمة
+    version='0.1',  # النسخة الحالية
+    packages=find_packages(),  # العثور على جميع الحزم داخل المجلد
+    install_requires=[  # الحزم المطلوبة
+        'tensorflow>=2.0',  # TensorFlow ضروري لتدريب النموذج
+        'opencv-python',  # OpenCV لمعالجة الصور
+        'numpy',  # Numpy للتعامل مع المصفوفات
     ],
+    description='An image classifier using MobileNetV2 for material prediction',  # وصف الحزمة
+    author='Ah777ed Said',  # اسم المؤلف
+    author_email='ahmed01226313482@gmail.com',  # البريد الإلكتروني للمؤلف
+    url='https://github.com/Ah777edSaid/MobileNet',  # رابط الريبو في GitHub
 )
